5d92250a69f5d21c27086a15f7447fc6d826625e
Karla Lopez <klo.lopez@gmail.com>
2015-06-04 20:37:33 -0700

    Seach

--- views.py
+++ views.py
@@ -95,38 +95,19 @@ def delete(id):
     return index()  # Look at the URL bar when you do this. What happens?
 
 
-@app.route('/search', methods=['GET', 'POST'])
+@app.route('/search', methods=['POST'])
 def search():
-
-    # We could define this inside its own function but it's simple enough
-    # that we don't really need to.
-
-    if request.method == 'GET':
-        return render_template('search.html')
 
     # Because we 'returned' for a 'GET', if we get to this next bit, we must
     # have received a POST
 
     term = request.form.get('term')
-    dessert = Dessert.query.filter_by(name=term)
+    dessert = Dessert.query.filter_by(name=term).first()
+    print
 
-
-    # Get the incoming data from the request.form dictionary.
-    # The values on the right, inside get(), correspond to the 'name'
-    # values in the HTML form that was submitted.
-
-    dessert_name = request.form.get('name_field')
-    dessert_price = request.form.get('price_field')
-    dessert_cals = request.form.get('cals_field')
-
-    # Now we are checking the input in create_dessert, we need to handle
-    # the Exception that might happen here.
-
-    # Wrap the thing we're trying to do in a 'try' block:
-    try:
-        dessert = edit_dessert(dessert, dessert_name, dessert_price, dessert_cals)
-        return render_template('edit.html', dessert=dessert)
-    except Exception as e:
+    if dessert:
+        return render_template('details.html', dessert=dessert)
+    else:
         # Oh no, something went wrong!
         # We can access the error message via e.message:
-        return render_template('edit.html', dessert=dessert, error=e.message)
+        return render_template('details.html', dessert=dessert, error="Nama does not exist")
